1db9c0bfa576b1cce2ea6abe5d6a4ce1e59c6c29
msusviela <msusviela@somniosoftware.com>
2026-05-16 18:49:53 -0300

    test(vscode-dashboard): include new tests in test runner

--- extensions/vscode-dashboard/src/test/run-tests.ts
+++ extensions/vscode-dashboard/src/test/run-tests.ts
@@ -1,9 +1,15 @@
 import { runDashboardMetricsTests } from './dashboardMetrics.test';
 import { runGitHubClientTests } from './githubClient.test';
+import { runFormattersTests } from './formatters.test';
+import { runCardsTests } from './cards.test';
+import { runGitHubClientExtraTests } from './githubClient.extra.test';
 
 async function main() {
   try {
     await runGitHubClientTests();
+    await runGitHubClientExtraTests();
+    await runFormattersTests();
+    await runCardsTests();
     await runDashboardMetricsTests();
     console.log('All unit tests passed');
     process.exit(0);
